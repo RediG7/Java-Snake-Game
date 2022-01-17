8360d4f6a3003715604ec270738909457824f155
RediG7 <guleksiredi7@gmail.com>
2022-01-18 00:48:13 +0100

    Initial Commit

---
+++ src/com/rediguleksi/Main.java
@@ -0,0 +1,11 @@
+package com.rediguleksi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // GameFrame frame = new GameFrame(); if we don't use the name, we just init
+        new GameFrame();
+
+    }
+}
